5c06a2395c0689d3976c5dd29af0b6fb63e1210b
andycarter-pe <civileng127@gmail.com>
2025-06-06 16:12:43 -0500

    config and s3 sub folder

--- src/push_to_s3_04.py
+++ src/push_to_s3_04.py
@@ -4,7 +4,7 @@
 #
 # Created by: Andy Carter, PE
 # Created - 2025.05.03
-# Revised - 2025.05.12 -- Smaller GeoJSON to TxDOT
+# Revised - 2025.06.06 -- Subfolder allowed on S3 -- publish_sub_folder
 # ************************************************************
 
 # ************************************************************
@@ -162,6 +162,11 @@ def fn_push_to_s3(str_config_file_path, b_print_output):
         section = config['write_to_s3']
         
         str_bucket_name = section.get('publish_bucket', '')
+        
+        # Handle optional publish_sub_folder
+        str_publish_sub_folder = section.get('publish_sub_folder', '').strip()
+        if str_publish_sub_folder and not str_publish_sub_folder.endswith('/'):
+            str_publish_sub_folder += '/'
     else:
         raise KeyError("Missing [write_to_s3] section in config file")
         
@@ -280,19 +285,19 @@ def fn_push_to_s3(str_config_file_path, b_print_output):
     
     
     # --- Write the bridge points ---
-    str_s3_bridge_pnt_key = 'bridge_warning_pnts.geojson'
+    str_s3_bridge_pnt_key = f"{str_publish_sub_folder}bridge_warning_pnts.geojson"
     fn_write_gdf_to_s3(gdf_s_bridge_warning_pnt, str_bucket_name, str_s3_bridge_pnt_key)
     
     # --- Write the navigation road lines ---
-    str_s3_road_ln_key = 'flood_road_nav_ln.geojson'
-    fn_write_gdf_to_s3(gdf_s_flood_road_nav_ln, str_bucket_name, str_s3_road_ln_key)
+    str_s3_road_nav_ln_key = f"{str_publish_sub_folder}flood_road_nav_ln.geojson"
+    fn_write_gdf_to_s3(gdf_s_flood_road_nav_ln, str_bucket_name, str_s3_road_nav_ln_key)
     
     # --- Write the trimmed road lines ---
-    str_s3_road_ln_key = 'flood_road_trim_ln.geojson'
-    fn_write_gdf_to_s3(gdf_s_flood_road_trim_ln, str_bucket_name, str_s3_road_ln_key)
+    str_s3_road_trim_ln_key = f"{str_publish_sub_folder}flood_road_trim_ln.geojson"
+    fn_write_gdf_to_s3(gdf_s_flood_road_trim_ln, str_bucket_name, str_s3_road_trim_ln_key)
     
     # --- Write the flood polygons ---
-    str_s3_flood_ar_key = 'flood_ar.geojson'
+    str_s3_flood_ar_key = f"{str_publish_sub_folder}flood_ar.geojson"
     fn_write_gdf_to_s3(gdf_s_flood_merge_ar, str_bucket_name, str_s3_flood_ar_key)
 # .........................................................
 
